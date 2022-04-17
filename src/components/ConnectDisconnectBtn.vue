953f55f5a147e750e4f54b9c8b0b38a7ec3c3740
evvvritt <evv.info@gmail.com>
2022-04-17 02:07:22 +0200

    fix btn icons

--- src/components/ConnectDisconnectBtn.vue
+++ src/components/ConnectDisconnectBtn.vue
@@ -5,14 +5,14 @@
     template(v-if="!$store.state.address")
       button.relative.w-full.mouse_hover_bg-yellow-600(@click="connectWallet")
         | {{ connectLbl }}
-        .absolute.top-0.right-0.h-full.flex.items-center.w-24.justify-center.pt-2 ꩜
+        .absolute.top-0.right-0.h-full.flex.items-center.justify-center.pt-2(:class="[iconWidth]") ꩜
 
     //- (disconnect btn)
     template(v-else)
       .relative.w-full.flex.items-center
         .w-full.text-center.pt-1
           addr(:address="$store.state.address")
-        button.absolute.top-0.right-0.h-full.flex.items-center.justify-center.mouse_hover_bg-yellow-600(@click="disconnectWallet", title="Disconnect", :class="[closeBtnWidth]")
+        button.absolute.top-0.right-0.h-full.flex.items-center.justify-center.mouse_hover_bg-yellow-600(@click="disconnectWallet", title="Disconnect", :class="[iconWidth]")
           svg-x.h-4.w-4(strokeWidth="1.1")
 </template>
 
@@ -23,7 +23,7 @@ export default {
   name: 'ConnectDisconnectBtn',
   props: {
     connectLbl: { type: String, default: 'Connect' },
-    closeBtnWidth: { type: String, default: 'w-24' }
+    iconWidth: { type: String, default: 'w-24' }
   },
   components: { Addr, SvgX },
   methods: {

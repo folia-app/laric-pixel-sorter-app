1940ee91d742aff8817c6ce94a27a9b4937a7dc8
evvvritt <evv.info@gmail.com>
2022-05-07 21:16:11 +0200

    back to direct load

--- src/components/MintImage.vue
+++ src/components/MintImage.vue
@@ -10,18 +10,18 @@
       //- (output image)
       template(v-if="loadOutput")
         //- w cloudinary
-        img.w-full.transition.duration-200(:src="`https://res.cloudinary.com/folia/image/fetch/https://decomposer.folia.app/api/${$store.state.networkId}/get/${mint.contractAddress}/${mint.tokenId}`", :class="{'opacity-0': !outputLoaded, 'hidden': outputLoaded === false}", @error="onOutputLoadError", @load="onOutputLoad",)
+        //- img.w-full.transition.duration-200(:src="`https://res.cloudinary.com/folia/image/fetch/https://decomposer.folia.app/api/${$store.state.networkId}/get/${mint.contractAddress}/${mint.tokenId}`", :class="{'opacity-0': !outputLoaded, 'hidden': outputLoaded === false}", @error="onOutputLoadError", @load="onOutputLoad",)
         //- direct
-        //- img.w-full.transition.duration-200(:src="`/api/${$store.state.networkId}/get/${mint.contractAddress}/${mint.tokenId}`", :class="{'opacity-0': !outputLoaded, 'hidden': outputLoaded === false}", @error="onOutputLoadError", @load="onOutputLoad",)
+        img.w-full.transition.duration-200(:src="`/api/${$store.state.networkId}/get/${mint.contractAddress}/${mint.tokenId}`", :class="{'opacity-0': !outputLoaded, 'hidden': outputLoaded === false}", @error="onOutputLoadError", @load="onOutputLoad",)
 
       //- (input image)
       //- * placeholder when outputLoaded = false
       //- * overlay when outputLoaded = true
       template(v-if="loadInput")
         //- cloudinary
-        img.w-full(:src="`https://res.cloudinary.com/folia/image/fetch/https://decomposer.folia.app/api/${$store.state.networkId}/get/original/${mint.contractAddress}/${mint.tokenId}`", :class="{'absolute overlay opacity-0 group-hover_opacity-100': outputLoaded, 'opacity-0': !inputLoaded, 'hidden': inputLoaded === false}", @load="onInputLoad", @error="onInputLoadError")
+        //- img.w-full(:src="`https://res.cloudinary.com/folia/image/fetch/https://decomposer.folia.app/api/${$store.state.networkId}/get/original/${mint.contractAddress}/${mint.tokenId}`", :class="{'absolute overlay opacity-0 group-hover_opacity-100': outputLoaded, 'opacity-0': !inputLoaded, 'hidden': inputLoaded === false}", @load="onInputLoad", @error="onInputLoadError")
         //- direct
-        //- img.w-full(:src="`/api/${$store.state.networkId}/get/original/${mint.contractAddress}/${mint.tokenId}`", :class="{'absolute overlay opacity-0 group-hover_opacity-100': outputLoaded, 'opacity-0': !inputLoaded, 'hidden': inputLoaded === false}", @load="onInputLoad", @error="onInputLoadError")
+        img.w-full(:src="`https://res.cloudinary.com/folia/image/fetch/https://decomposer.folia.app/api/${$store.state.networkId}/get/original/${mint.contractAddress}/${mint.tokenId}`", :class="{'absolute overlay opacity-0 group-hover_opacity-100': outputLoaded, 'opacity-0': !inputLoaded, 'hidden': inputLoaded === false}", @load="onInputLoad", @error="onInputLoadError")
 
     //- (loading overlay text)
     template(v-if="outputLoaded === false")
